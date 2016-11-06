20a57b9fc41b2e63f8cb121d7d5315c9087a1f95
Alexis Métaireau <alexis@notmyidea.org>
2016-11-06 18:58:13 +0100

    Add a few options

--- extractor.py
+++ extractor.py
@@ -1,114 +1,132 @@
+import argparse
 import copy
 import json
 import os
 import requests
 import shutil
-import sys
 import urlparse
 
 from jinja2 import FileSystemLoader, Environment
 
 __HERE__ = os.path.dirname(os.path.abspath(__file__))
-TEMPLATES_FOLDER = os.path.join(__HERE__, "templates")
+TEMPLATES_FOLDER = os.path.join(__HERE__, 'templates')
 
 
-def transform_metadata(url, dest_path):
-    resp = requests.get(url)
-    resp.raise_for_status()
+class VideoDownloader(object):
 
-    origin = resp.json()['entity']
-    dest = copy.deepcopy(origin)
+    def __init__(self, metadata_dir, html_dir='.', verbose=True,
+                 dry_run=False):
+        self.metadata_dir = metadata_dir
+        self.html_dir = html_dir
+        self.verbose = verbose
+        self.dry_run = dry_run
 
-    timecodes = origin['timecodes']
-    video_id = origin['id']
+    def log(self, message):
+        if self.verbose:
+            print(message)
 
-    local_folder = os.path.join(dest_path, video_id)
-    if not os.path.exists(local_folder):
-        os.makedirs(local_folder)
+    def transform_metadata(self, url):
+        resp = requests.get(url)
+        resp.raise_for_status()
 
-    def transform_timecodes(timecode):
-        small_url = urlparse.urljoin(url, timecode['image']['small'])
-        large_url = urlparse.urljoin(url, timecode['image']['large'])
-        filename = urlparse.urlparse(small_url).path.split('/')[-1]
-        name, ext = os.path.splitext(filename)
-        small_filename = os.path.join(local_folder, '%s-small%s' % (name, ext))
-        large_filename = os.path.join(local_folder, '%s-large%s' % (name, ext))
-        timecode['image']['small'] = download_image(small_url, small_filename)
-        timecode['image']['large'] = download_image(large_url, large_filename)
-        return timecode
+        origin = resp.json()['entity']
+        dest = copy.deepcopy(origin)
 
-    dest['timecodes'] = map(transform_timecodes, timecodes)
-    thumbnail_url = urlparse.urljoin(url, origin["thumbnail"])
-    thumbnail_filename = os.path.join(local_folder, 'thumbnail.jpg')
-    dest['thumbnail'] = download_image(thumbnail_url, thumbnail_filename)
+        timecodes = origin['timecodes']
+        video_id = origin['id']
 
-    # origin['link'] should be deleted?
+        local_folder = os.path.join(self.metadata_dir, video_id)
+        if not (self.dry_run and os.path.exists(local_folder)):
+            os.makedirs(local_folder)
 
-    return dest
+        def transform_timecodes(timecode):
+            small_url = urlparse.urljoin(url, timecode['image']['small'])
+            large_url = urlparse.urljoin(url, timecode['image']['large'])
+            filename = urlparse.urlparse(small_url).path.split('/')[-1]
+            name, ext = os.path.splitext(filename)
+            small_fn = os.path.join(local_folder, '%s-small%s' % (name, ext))
+            large_fn = os.path.join(local_folder, '%s-large%s' % (name, ext))
+            image = timecode['image']
+            image['small'] = self.download_image(small_url, small_fn)
+            image['large'] = self.download_image(large_url, large_fn)
+            return timecode
+
+        dest['timecodes'] = map(transform_timecodes, timecodes)
+        thumbnail_url = urlparse.urljoin(url, origin['thumbnail'])
+        thumbnail_fn = os.path.join(local_folder, 'thumbnail.jpg')
+        dest['thumbnail'] = self.download_image(thumbnail_url, thumbnail_fn)
+
+        return dest
+
+    def download_image(self, image_url, destination_path):
+        self.log('Downloading %s at %s' % (image_url, destination_path))
+        if not self.dry_run:
+            resp = requests.get(image_url, stream=True)
+            if resp.status_code == 200:
+                with open(destination_path, 'wb') as f:
+                    resp.raw_decode_content = True
+                    shutil.copyfileobj(resp.raw, f)
+        return destination_path
+
+    def download_slides(self, url):
+        metadata = self.transform_metadata(url)
+        filename = '%s.json' % metadata['id']
+        if not self.dry_run:
+            with open(os.path.join(self.metadata_dir, filename), 'w+') as f:
+                json.dump(metadata, f)
+        return filename, metadata
+
+    def copy_templates(self, filename, metadata):
+        simple_loader = FileSystemLoader(TEMPLATES_FOLDER)
+        env = Environment(loader=simple_loader)
+        template = env.get_template('index.html')
+        rendered = template.render(metadata=metadata, filename=filename)
+        dest_file = os.path.join(self.html_dir, metadata['id'] + '.html')
+        if not self.dry_run:
+            with open(dest_file, 'w+') as f:
+                f.write(rendered)
+
+    def get_metadata_url_from_video_url(self, video_url):
+        _id = urlparse.urlparse(video_url).path.split('/')[-1]
+        metadata_url = 'http://www.demo.openveo.com/publish/getVideo/%s' % _id
+        return metadata_url
+
+    def generate_video_html(self, video_url):
+        try:
+            metadata_url = self.get_metadata_url_from_video_url(video_url)
+            filename, metadata = self.download_slides(metadata_url)
+            self.copy_templates(filename, metadata)
+        except Exception as e:
+            self.log(str(e))
 
 
-def download_image(url, path):
-    print("Downloading %s at %s" % (url, path))
-    resp = requests.get(url, stream=True)
-    if resp.status_code == 200:
-        with open(path, 'wb') as f:
-            resp.raw_decode_content = True
-            shutil.copyfileobj(resp.raw, f)
-    return path
-
-
-def download_slides(url, output_dir):
-    metadata = transform_metadata(url, output_dir)
-
-    filename = '%s.json' % metadata['id']
-    with open(os.path.join(output_dir, filename), 'w+') as f:
-        json.dump(metadata, f)
-
-    return filename, metadata
-
-
-def copy_templates(output_dir, filename, metadata):
-    simple_loader = FileSystemLoader(TEMPLATES_FOLDER)
-    env = Environment(loader=simple_loader)
-    template = env.get_template("index.html")
-    rendered = template.render(metadata=metadata, filename=filename)
-    destination_file = os.path.join(output_dir, metadata['id'] + '.html')
-    with open(destination_file, "w+") as f:
-        f.write(rendered)
-
-
-def get_metadata_url_from_video_url(video_url):
-    _id = urlparse.urlparse(video_url).path.split('/')[-1]
-    metadata_url = 'http://www.demo.openveo.com/publish/getVideo/%s' % _id
-    return metadata_url
-
-
-def generate_video_html(video_url, metadata_dir, html_dir="."):
-    metadata_url = get_metadata_url_from_video_url(video_url)
-    filename, metadata = download_slides(metadata_url, metadata_dir)
-    if filename and metadata:
-        copy_templates(html_dir, filename, metadata)
-
-
-def parse_json(json_file):
+def get_list_of_urls(json_file):
     with open(json_file) as f:
         data = json.load(f)
     return ['http://www.demo.openveo.com' + r['link']
             for r in data['rows']]
 
-def main(json_file, metadata_dir, html_dir):
-    urls = parse_json(json_file)
-    print "Downloading %s videos" % len(urls)
+
+def main(json_file, metadata_dir, html_dir, verbose=None, dry_run=None):
+    downloader = VideoDownloader(metadata_dir, html_dir, verbose, dry_run)
+    urls = get_list_of_urls(json_file)
+    downloader.log('Downloading %s videos' % len(urls))
     for url in urls:
-        try:
-            generate_video_html(url, metadata_dir, html_dir)
-        except Exception as e:
-            print str(e)
+        downloader.generate_video_html(url)
+
 
 if __name__ == '__main__':
-    if len(sys.argv) < 2:
-        exit('Please specify the URL of the openveo JSON file to extract')
-    url = sys.argv[1]
-    metadata_dir = sys.argv[2] if len(sys.argv) >= 3 else '.'
-    html_dir = sys.argv[3] if len(sys.argv) >= 4 else '.'
-    main(url, metadata_dir, html_dir)
+    description = 'Download and host openveo videos on a static website.'
+    parser = argparse.ArgumentParser(description=description)
+    parser.add_argument(
+        'json_file',
+        help='JSON file containing the list of videos')
+    parser.add_argument(
+        'metadata_dir',
+        help='Path to the dir where metadata files will be written')
+    parser.add_argument('--html-dir', default='.')
+    parser.add_argument('--quiet', action='store_true', default=False)
+    parser.add_argument('--dry-run', action='store_true', default=False)
+    args = parser.parse_args()
+    main(args.json_file, args.metadata_dir, args.html_dir, not args.quiet,
+         args.dry_run)
